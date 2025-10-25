73d3076adf3d7c6092d5c2c24f315dc337d2a183
Caio Santos Vargas <caio.vargas@hotmail.com>
2025-10-24 23:50:25 -0300

    dash sub

--- admin/_sis/wc_menu.php
+++ admin/_sis/wc_menu.php
@@ -10,12 +10,28 @@
         <span class="menu-text">Meus Clientes / LEADs</span>
     </a>
 </li>
-<li class="dashboard_nav_menu_li <?= strstr($getViewInput, 'organizacao/home') ? 'active' : ''; ?>">
+<!-- <li class="dashboard_nav_menu_li <?= strstr($getViewInput, 'organizacao/home') ? 'active' : ''; ?>">
     <a title="Leads" href="dashboard.php?wc=organizacao/home" data-tooltip="Leads">
         <i class="icon-office"></i>
         <span class="menu-text">Minhas Empresas</span>
     </a>
 </li>
+ -->
+<li class="dashboard_nav_menu_li has-submenu <?= strstr($getViewInput, 'organizacao/home') ? 'active' : ''; ?>">
+    <a href="#" data-tooltip="Portais">
+       <i class="icon-office"></i>
+        <span class="menu-text">Minhas Empresas</span>
+        <div class="submenu-arrow">
+            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5303 9.46967C18.8232 9.76256 18.8232 10.2374 18.5303 10.5303L12.5303 16.5303C12.2374 16.8232 11.7626 16.8232 11.4697 16.5303L5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967C5.76256 9.17678 6.23744 9.17678 6.53033 9.46967L12 14.9393L17.4697 9.46967C17.7626 9.17678 18.2374 9.17678 18.5303 9.46967Z" fill="#030D45" />
+            </svg>
+        </div>
+    </a>
+    <ul class="dashboard_nav_menu_sub">
+        <li><a href="dashboard.php?wc=organizacao/home"> Equipe </a></li>
+    </ul>
+</li>
+
 <li class="dashboard_nav_menu_li <?= strstr($getViewInput, 'especialista/create') ? 'active' : ''; ?>">
     <a title="Especialista Associado" href="dashboard.php?wc=especialista/create" data-tooltip="Especialista">
         <span style="display: flex;gap: 12px;">
