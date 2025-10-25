e9ed7b4ab3bead2811dbde124482b759f3616d7b
Caio Santos Vargas <caio.vargas@hotmail.com>
2025-10-25 00:02:01 -0300

    home

--- admin/_sis/home1.php
+++ admin/_sis/home1.php
@@ -12,7 +12,7 @@
 <div style="position: relative; width: 100%; height: 100vh;">
   <iframe 
       src="https://parceiro-residere-interno-pag-painel.lovable.app/" 
-      style="position:absolute; top:0; left:0; width:100%; height:100%; border:none; border-radius:10px;" 
+      style="position:absolute; top:0; left:0; width:100%; height:100%; border:none;" 
       allowfullscreen 
       loading="lazy">
   </iframe>
