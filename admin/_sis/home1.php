91644b966ba402c1709b512d5fe64df85c4fd73e
Caio Santos Vargas <caio.vargas@hotmail.com>
2025-10-24 21:23:35 -0300

    pagina

--- admin/_sis/home1.php
+++ admin/_sis/home1.php
@@ -9,10 +9,19 @@
     </div>
 </header>
 
-<div class="dashboard_content">
+<div style="position: relative; width: 100%; height: 100vh;">
+  <iframe 
+      src="https://parceiro-residere-interno-pag-painel.lovable.app/" 
+      style="position:absolute; top:0; left:0; width:100%; height:100%; border:none; border-radius:10px;" 
+      allowfullscreen 
+      loading="lazy">
+  </iframe>
+</div>
+
+<!-- <div class="dashboard_content">
     <div class="box box60" style="text-align: center">
         <iframe id="panda-c1efedbb-4798-4b15-a630-7076dcff826d" src="https://player-vz-a527f663-fed.tv.pandavideo.com.br/embed/?v=c1efedbb-4798-4b15-a630-7076dcff826d" style="border:none; width: 844px; height: 475px" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture" allowfullscreen=true fetchpriority="high"></iframe>
     </div>
     <div class="box box40">
     </div>
-</div>
+</div> -->
