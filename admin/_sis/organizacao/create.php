9186fd0eb416f69cc0e0a52960e416e7082d5365
Wellington Junior <junior@mxse.com.br>
2025-10-24 22:05:38 -0300

    Gestão de Organização

---
+++ admin/_sis/organizacao/create.php
@@ -0,0 +1,84 @@
+<?php
+$RegId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+if ($RegId):
+    $Read->ExeRead(DB_USERS, "WHERE user_id = {$RegId} AND user_id_principal={$Admin["user_id"]}", "");
+    if ($Read->getResult()):
+        $FormData = array_map('htmlspecialchars', $Read->getResult()[0]);
+        extract($FormData);
+    else:
+        $_SESSION['trigger_controll'] = Erro("<b>OPPSS {$Admin['user_name']}</b>, você tentou editar um registro que não existe ou que foi removido recentemente!", E_USER_NOTICE);
+        header('Location: dashboard.php?wc=organizacao/home');
+        exit;
+    endif;
+else:
+    $RegCreate = [
+        'user_registration' => date('Y-m-d H:i:s'),
+        'user_status' => 1,
+        'user_id_principal' => $Admin['user_id'],
+        "user_level" => 20
+    ];
+    $Create->ExeCreate(DB_USERS, $RegCreate);
+    $user_id = $Create->getResult();
+    if ($Create->getResult()):
+        header("Location: dashboard.php?wc=organizacao/create&id={$user_id}");
+        exit;
+    endif;
+endif;
+?>
+
+<header class="dashboard_header">
+    <div class="dashboard_header_title">
+        <h1 class="icon-office">Minhas Empresas ou Equipes</h1>
+        <p class="dashboard_header_breadcrumbs">
+            <a title="<?= SITE_NAME2; ?>" href="dashboard.php?wc=home">Dashboard</a>
+            <span class="crumb">/</span>
+            <a title="<?= SITE_NAME2; ?>" href="dashboard.php?wc=organizacao/home">Empresas ou Equipes</a>
+            <span class="crumb">/</span>
+            <?= $user_name ? $user_name : "Nova Empresa ou Equipe" ?>
+        </p>
+    </div>
+
+    <div class="dashboard_header_search" style="font-size: 0.875em; margin-top: 16px;" id="<?= $RegId; ?>">
+        <span rel='dashboard_header_search' class='j_delete_action icon-warning btn btn_red' id='<?= $RegId; ?>'>Deletar</span>
+        <span rel='dashboard_header_search' callback="Organizacao" callback_action='delete' class='j_delete_action_confirm icon-warning btn btn_yellow' style='display: none' id='<?= $RegId; ?>'>EXCLUIR AGORA!</span>
+    </div>
+</header>
+
+<div class="dashboard_content">
+    <div class="box box70">
+        <?php
+        require("inc.profile.php");
+        ?>
+    </div>
+</div>
+
+<style>
+    .avatar_union {
+        display: flex;
+        align-items: center;
+        position: relative;
+        gap: 0;
+        background: #eee;
+        padding: 20px;
+    }
+
+    .avatar {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        overflow: hidden;
+        border: 3px solid #fff;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+    }
+
+    .avatar img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .avatar.overlap {
+        margin-left: -20px;
+        z-index: 2;
+    }
+</style>
--- admin/_sis/wc_menu.php
+++ admin/_sis/wc_menu.php
@@ -1,41 +1,40 @@
-<li class="dashboard_nav_menu_li <?= $getViewInput == 'home' ? 'dashboard_nav_menu_active' : ''; ?>">
-    <a class="icon-home" title="Dashboard" href="dashboard.php?wc=home">Dashboard</a>
+<li class="dashboard_nav_menu_li">
+    <a title="Dashboard" href="dashboard.php?wc=home" data-tooltip="Dashboard">
+        <i class="icon-home"></i>
+        <span class="menu-text">Página Inicial</span>
+    </a>
 </li>
 <li class="dashboard_nav_menu_li">
-    <a title="Especialista" href="dashboard.php?wc=especialista/create" data-tooltip="Especialista">
-        <i class="icon-user"></i>
-        <span class="menu-text">Especialista</span>
-    </a>
-</li>
-<li class="dashboard_nav_menu_li has-submenu">
     <a title="Leads" href="dashboard.php?wc=leads/home" data-tooltip="Leads">
         <i class="icon-users"></i>
-        <span class="menu-text">Leads</span>
-        <div class="submenu-arrow">
-            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5303 9.46967C18.8232 9.76256 18.8232 10.2374 18.5303 10.5303L12.5303 16.5303C12.2374 16.8232 11.7626 16.8232 11.4697 16.5303L5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967C5.76256 9.17678 6.23744 9.17678 6.53033 9.46967L12 14.9393L17.4697 9.46967C17.7626 9.17678 18.2374 9.17678 18.5303 9.46967Z" fill="#030D45" />
-            </svg>
-        </div>
+        <span class="menu-text">Meus Clientes / LEADs</span>
     </a>
-    <ul class="dashboard_nav_menu_sub">
-        <li><a title="Ver Leads" href="dashboard.php?wc=leads/home">&raquo; Ver Leads</a></li>
-        <li><a title="Novo Lead" href="dashboard.php?wc=leads/create">&raquo; Novo Lead</a></li>
-    </ul>
 </li>
+<li class="dashboard_nav_menu_li">
+    <a title="Leads" href="dashboard.php?wc=organizacao/home" data-tooltip="Leads">
+        <i class="icon-office"></i>
+        <span class="menu-text">Minhas Empresas</span>
+    </a>
+</li>
+<li class="dashboard_nav_menu_li">
+    <a title="Especialista Associado" href="dashboard.php?wc=especialista/create" data-tooltip="Especialista">
+        <i class="icon-user"></i>
+        <span class="menu-text">Falar com a Residere</span>
+    </a>
+</li>
+<?php /*. 
+<li class="dashboard_nav_menu_li">
+    <a target="_New" title="ResiH" href="https://ead.resiplace.com.br" data-tooltip="Academia">
+        <i class="icon-books"></i>
+        <span class="menu-text">Aprender e Crescer</span>
+    </a>
+</li> */?>
 <li class="dashboard_nav_menu_li">
     <a target="_New" title="ResiH" href="https://resiplace.com.br" data-tooltip="ResiPlace">
         <i class="icon-star-full"></i>
         <span class="menu-text">ResiPlace</span>
     </a>
 </li>
-<?php /*
-<li class="dashboard_nav_menu_li">
-    <a target="_New" title="ResiH" href="https://ead.resiplace.com.br" data-tooltip="Academia">
-        <i class="icon-books"></i>
-        <span class="menu-text">Treinamentos</span>
-    </a>
-</li> */?>
-
 <script>
     document.querySelectorAll(".dashboard_nav_menu_toggle").forEach(toggle => {
         toggle.addEventListener("click", function() {
